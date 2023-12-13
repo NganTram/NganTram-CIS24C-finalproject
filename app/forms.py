340315856e1dacfa2a5653dd2dd7ee920b301cd2
NganTram <34253674+NganTram@users.noreply.github.com>
2023-12-12 19:26:58 -0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms import StringField, validators
 
 class TrigForm(FlaskForm):
     function_name = SelectField('Function', 
                                 choices=[('sin', 'Sin'), ('cos', 'Cos'), ('tan', 'Tan'), ('cot', 'Cot')],
                                 validators=[DataRequired()])
-    action = SelectField('Action', 
+    custom_function = StringField('Custom Function', validators=[validators.Optional()])
+    action = SelectField('Action',
                          choices=[('plot', 'Plot Function'), ('inverse', 'Plot Inverse'), ('domain_range', 'Find Domain and Range'), ('asymptotes', 'Find Asymptotes')],
                          validators=[DataRequired()])
     submit = SubmitField('Visualize')
